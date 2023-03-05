ebd62ced7e4278abe6d5b09fd095939caae12f6b
skanae <kanae11277@gmail.com>
2023-03-05 20:01:50 +0900

    one integer compiler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("引数の個数が正しくありません");
+    }
+    
+      println!(".intel_syntax noprefix");
+      println!(".globl main");
+      println!("main:");
+      println!("  mov rax, {}", args[1].to_string());
+      println!("  ret");
 }
